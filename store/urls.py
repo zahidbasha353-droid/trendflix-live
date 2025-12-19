f91af8eb01840fed63f2505e45ab7afa8ab589e5
zahidbasha <zahidbasha353@gmail.com>
2025-12-19 20:13:26 +0530

    Fixed NoReverseMatch by renaming cart and checkout URLs

--- store/urls.py
+++ store/urls.py
@@ -51,10 +51,10 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    path('cart/', views.cart_view, name='cart_view'),
+    path('cart/', views.cart_view, name='cart'),
     path('buy-now/<int:product_id>/', views.buy_now, name='buy_now'),
     path('remove-from-cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
-    path('checkout/', views.checkout_view, name='checkout_view'),
+    path('checkout/', views.checkout_view, name='checkout'),
     
     # Inga dhaan prachanai - 'order_success_view' nu function irukanum
     path('order-success/', views.order_success_view, name='order_success_view'),
